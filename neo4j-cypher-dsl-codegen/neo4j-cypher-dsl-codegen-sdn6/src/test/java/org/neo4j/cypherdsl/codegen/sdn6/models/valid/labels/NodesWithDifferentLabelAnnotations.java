b01cdc29e5980afa7aee579f274b8bb787fb384c
Michael Simons <michael.simons@neo4j.com>
2021-03-05 15:48:51 +0100

    GH-156 - Create an annotation processor for Spring Data Neo4j 6.
    This commit creates an annotation processor understanding Spring Data Neo4j 6 and generates a model reassembling what has been introduced with 1c59f313ef0a58d1a7ffbd27b5619e25af2b995f. It requires SDN 6 on the classpath when applied in a project.
    
    Documentation will follow in another change.
    
    This closes #156.

---
+++ neo4j-cypher-dsl-codegen/neo4j-cypher-dsl-codegen-sdn6/src/test/java/org/neo4j/cypherdsl/codegen/sdn6/models/valid/labels/NodesWithDifferentLabelAnnotations.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2019-2021 "Neo4j,"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.cypherdsl.codegen.sdn6.models.valid.labels;
+
+import org.springframework.data.neo4j.core.schema.Node;
+
+/**
+ * Tests various ways to define labels. The generated classes will end up in a subpackage as they are static, inner classes.
+ *
+ * @author Michael J. Simons
+ */
+public class NodesWithDifferentLabelAnnotations {
+
+	@Node("ALabel")
+	static class LabelOnNode1 {
+	}
+
+	@Node(value = "ALabel")
+	static class LabelOnNode2 {
+	}
+
+	@Node(primaryLabel = "ALabel")
+	static class LabelOnNode3 {
+	}
+
+	@Node(value = { "Label1", "Label2" })
+	static class MultipleLabels1 {
+	}
+
+	@Node(primaryLabel = "PL", value = { "Label1", "Label2" })
+	static class MultipleLabels2 {
+	}
+
+	@Node(primaryLabel = "PL", value = { "Label1", "Label2" }, labels = "Some more")
+	static class MultipleLabels3 {
+	}
+}
