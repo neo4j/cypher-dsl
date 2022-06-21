d88bb6e5f393a7ab4abe4bb8398b6ca88de4c7e7
Michael Simons <michael.simons@neo4j.com>
2022-06-21 13:22:53 +0200

    Add some written docs.

--- neo4j-cypher-dsl-examples/neo4j-cypher-dsl-examples-sdn6/src/main/java/org/neo4j/cypherdsl/examples/sdn6/movies/PeopleService.java
+++ neo4j-cypher-dsl-examples/neo4j-cypher-dsl-examples-sdn6/src/main/java/org/neo4j/cypherdsl/examples/sdn6/movies/PeopleService.java
@@ -129,19 +129,21 @@ final class PeopleService {
 	}
 	// end::using-person-repo[]
 
+	// tag::using-temporals[]
 	Optional<Person> createNewPerson(NewPersonCmd newPersonCmd) {
 		var p = Person_.PERSON.withProperties(
-			Person_.PERSON.NAME, Cypher.anonParameter(newPersonCmd.getName())
+			Person_.PERSON.NAME, Cypher.anonParameter(newPersonCmd.getName()) // <.>
 		).named("p");
 
 		var statement = Cypher.merge(p)
 			.onCreate().set(
 				p.BORN, Cypher.parameter("arbitraryName")
-					.withValue(newPersonCmd.getDob().getYear()),
-				p.DOB, Cypher.anonParameter(newPersonCmd.getDob())
+					.withValue(newPersonCmd.getDob().getYear()), // <.>
+				p.DOB, Cypher.anonParameter(newPersonCmd.getDob()) // <.>
 			).returning(p).build();
 		return peopleRepository.findOne(statement);
 	}
+	// end::using-temporals[]
 
 	// tag::using-person-repo[]
 }
