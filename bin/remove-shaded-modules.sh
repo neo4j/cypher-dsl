9ca1598ee6801ae50e697382dcefaba65efdb23b
Michael Simons <michael.simons@neo4j.com>
2023-01-19 22:22:57 +0100

    build: Move the fixed `module-info.java` somewhere out of IDEAs reach.

--- bin/remove-shaded-modules.sh
+++ bin/remove-shaded-modules.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 DIR="$(dirname "$(realpath "$0")")"
 
-mkdir -p $DIR/../neo4j-cypher-dsl/target/generated-sources/misc
-sed 's/requires org.neo4j.cypherdsl.support.schema_name;$//g'  $DIR/../neo4j-cypher-dsl/src/main/java/module-info.java >  $DIR/../neo4j-cypher-dsl/target/generated-sources/misc/module-info.java
+mkdir -p $DIR/../neo4j-cypher-dsl/target/modules
+sed 's/requires org.neo4j.cypherdsl.support.schema_name;$//g'  $DIR/../neo4j-cypher-dsl/src/main/java/module-info.java >  $DIR/../neo4j-cypher-dsl/target/modules/module-info.java
