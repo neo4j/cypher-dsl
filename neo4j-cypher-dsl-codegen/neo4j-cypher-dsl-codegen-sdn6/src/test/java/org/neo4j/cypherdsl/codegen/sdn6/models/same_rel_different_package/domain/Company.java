ad15f9063817b6f64c5ec6e12d0a8fb24efb8613
Michael Simons <michael.simons@neo4j.com>
2025-10-08 10:06:46 +0200

    What am I even doing here.

--- neo4j-cypher-dsl-codegen/neo4j-cypher-dsl-codegen-sdn6/src/test/java/org/neo4j/cypherdsl/codegen/sdn6/models/same_rel_different_package/domain/Company.java
+++ neo4j-cypher-dsl-codegen/neo4j-cypher-dsl-codegen-sdn6/src/test/java/org/neo4j/cypherdsl/codegen/sdn6/models/same_rel_different_package/domain/Company.java
@@ -18,8 +18,6 @@
  */
 package org.neo4j.cypherdsl.codegen.sdn6.models.same_rel_different_package.domain;
 
-import org.neo4j.cypherdsl.codegen.sdn6.models.same_rel_different_package.application.PlaceModel;
-
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Relationship;
 
